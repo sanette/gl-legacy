0ec05f481cffe81af89ef5cf88ab0f6ad58382c6
sanette <sanette-linux@laposte.net>
2025-09-23 11:07:53 +0200

    blind test

---
+++ test/blind.ml
@@ -0,0 +1,28 @@
+(* Blind test. We don't create an openGL context, so nothing will be drawn. *)
+
+module Gl = Gl_legacy
+
+let () =
+(* Clear screen to black *)
+    Gl.clear_color 0.0 0.0 0.0 1.0;
+    Gl.clear Gl.color_buffer_bit;
+
+    (* Projection *)
+    Gl.matrix_mode Gl.projection;
+    Gl.load_identity ();
+    Gl.ortho (-1.0) 1.0 (-1.0) 1.0 (-1.0) 1.0;
+
+    (* Modelview *)
+    Gl.matrix_mode Gl.modelview;
+    Gl.load_identity ();
+    Gl.rotatef 45. 0.0 0.0 1.0;
+
+    (* Triangle *)
+    Gl.gl_begin Gl.triangles;
+    Gl.color3f 1.0 0.0 0.0;
+    Gl.vertex2f 0.0 0.8;
+    Gl.color3f 0.0 1.0 0.0;
+    Gl.vertex2f (-0.8) (-0.8);
+    Gl.color3f 0.0 0.0 1.0;
+    Gl.vertex2f 0.8 (-0.8);
+    Gl.gl_end ();
