75b5c0fc9ad571b30acfd3d221c7e201760d8320
sanette <sanette-linux@laposte.net>
2026-01-12 09:01:09 +0100

    print OCAMLCI

--- test/test.blind.ml
+++ test/test.blind.ml
@@ -1,2 +1,5 @@
 let () =
-  print_endline "Tests skipped. Please install tsdl."
+  print_endline "Tests skipped. Please install tsdl.";
+  match Sys.getenv_opt "OCAMLCI" with
+  | Some s -> print_endline ("OCAMLC=" ^ s)
+  | None -> print_endline "OCAMLC not set."
